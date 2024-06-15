2f2e04d6df5d46ca328904166784940f34dc068f
Rob <gubinrobert@gmail.com>
2024-06-16 01:50:56 +0200

    like|dislike commands

--- chat.py
+++ chat.py
@@ -45,6 +45,12 @@ class ChatHandler:
     async def reset(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         context.user_data[ChatHandler.message_history_key] = [self.system_prompt]
 
+    async def like(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        logging.info("Sending conversation history for preference analysis")
+
+    async def dislike(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        logging.info("Sending conversation history for hatred analysis")
+
     async def respond(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         await self.typing(update, context)
         user_message_text = update.message.text
@@ -55,7 +61,7 @@ class ChatHandler:
 
         messages_history = self._update_and_get(
             [
-                llm.create_user_message(update.message.text),
+                llm.create_user_message(user_message_text),
                 llm.create_system_message(
                     prompts.mk_instructions_search_prompt(instruction_search_result)
                 ),
@@ -121,6 +127,12 @@ def main():
 
     reset_handler = CommandHandler("reset", chat_handler.reset)
     application.add_handler(reset_handler)
+
+    like_handler = CommandHandler("like", chat_handler.like)
+    application.add_handler(like_handler)
+
+    dislike_handler = CommandHandler("dislike", chat_handler.dislike)
+    application.add_handler(dislike_handler)
 
     message_handler = MessageHandler(
         filters.TEXT & (~filters.COMMAND), chat_handler.respond
